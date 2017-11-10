c23ad3c8bc2e55590d0faecf4a771e46bec67125
Afzal Ashraf <afzalashraf.dev@gmail.com>
2017-11-11 01:34:08 +0500

    Admin Drawer component completed with functionality

--- src/components/AdminDrawer.js
+++ src/components/AdminDrawer.js
@@ -65,6 +65,145 @@ class AdminDrawer extends React.Component {
         }));
     }
 
+    openAddMember = () => {
+        this.setState({
+            isDrawerOpen: false,
+            memberIndexForAssignTask: -1,
+            memberIndexForViewAllTasks: -1,
+            selectedMemberIndex: -1,
+            memberToBeCreated: {
+                name: '',
+                username: '',
+                email: '',
+                password: '',
+                phoneNumber: '',
+                address: '',
+                vehicleType: '',
+            },
+        });
+    }
+
+
+    openEditMember = (selectedMemberIndex) => {
+        this.setState({
+            isDrawerOpen: false,
+            selectedMemberIndex,
+            memberIndexForAssignTask: -1,
+            memberIndexForViewAllTasks: -1,
+        });
+    }
+
+    addMember = (member) => {
+        this.props.setLoader(true);
+
+        this.props.addMember(member).then((res) => {
+            if (res.status === 200) {
+                const members = this.state.members;
+                members.push(res.data.data.member);
+                this.setState({
+                    members,
+                    memberToBeCreated: null,
+                }, () => {
+                    this.props.setLoader(false);
+                    this.props.setResponseMsg(res.data.data.msg);
+                });
+            }
+        }).catch((err) => {
+            this.props.setLoader(false);
+            this.props.setResponseMsg(err.response.data.msg);
+        });
+    }
+
+    closeAddEditMember = () => {
+        this.setState({
+            selectedMemberIndex: -1,
+            memberToBeCreated: null,
+        });
+    }
+
+    saveEditMember = (member) => {
+        this.props.setLoader(true);
+
+        this.props.editMember(member).then((res) => {
+            if (res.status === 200) {
+                const members = this.state.members;
+                members[this.state.selectedMemberIndex] = res.data.data.member;
+        
+                this.setState({
+                    members,
+                    selectedMemberIndex: -1,
+                }, () => {
+                    this.props.setLoader(false);
+                    this.props.setResponseMsg(res.data.data.msg);
+                });
+            }
+        }).catch((err) => {
+            this.props.setLoader(false);
+            this.props.setResponseMsg(err.response.data.msg);
+        });
+    }
+
+    deleteMember = (memberId, index) => {
+        this.props.setLoader(true);
+
+        this.props.deleteMember(memberId).then((res) => {
+            if (res.status === 200) {
+                const members = this.state.members;
+                members.splice(index, 1);
+        
+                this.setState({
+                    members,
+                }, () => {
+                    this.props.setLoader(false);
+                    this.props.setResponseMsg(res.data.data.msg);
+                });
+            }
+        }).catch((err) => {
+            this.props.setLoader(false);
+            this.props.setResponseMsg(err.response.data.msg);
+        });
+    }
+
+    openAssignTask = (memberIndexForAssignTask) => {
+        this.setState({
+            isDrawerOpen: false,
+            memberIndexForAssignTask,
+            memberIndexForViewAllTasks: -1,
+            selectedMemberIndex: -1,
+        });
+    }
+
+    closeAssignTask = () => {
+        this.setState({
+            memberIndexForAssignTask: -1,
+        }, () => {
+            this.props.saveMarkers([]);
+        });
+    }
+
+    assignTask = (task) => {
+        this.props.setLoader(true);
+
+        this.props.addTask(task).then((res) => {
+            if (res.status === 200) {
+                this.closeAssignTask();
+                this.props.setLoader(false);
+            }
+        }).catch((err) =>{
+            this.props.setLoader(false);
+            this.props.setResponseMsg(err.response.data.msg);
+        });
+    }
+
+    toggleViewAllTasks = (memberIndexForViewAllTasks) => {
+        this.setState({
+            isDrawerOpen: false,
+            memberIndexForViewAllTasks,
+            memberIndexForAssignTask: -1,
+            selectedMemberIndex: -1,
+        });
+    }
+
     render() {
         return (
             <div className="drawer-container">
@@ -117,6 +256,35 @@ class AdminDrawer extends React.Component {
                         <FontIcon className="zmdi zmdi-caret-right"/>
                     </div>
                 </div>
+                
+                {
+                    (this.state.selectedMemberIndex !== -1 || this.state.memberToBeCreated) &&
+                    <AddEditMember
+                        member={this.state.memberToBeCreated ? this.state.memberToBeCreated : this.state.members[this.state.selectedMemberIndex]}
+                        isAddMember={this.state.memberToBeCreated !== null}
+                        close={this.closeAddEditMember}
+                        edit={this.saveEditMember}
+                        add={this.addMember}
+                    />
+                }
+
+                {
+                    this.state.memberIndexForAssignTask !== -1 &&
+                    <AssignTask
+                        memberIndexForAssignTask={this.state.memberIndexForAssignTask}
+                        members={this.state.members}
+                        assign={this.assignTask}
+                        close={this.closeAssignTask}
+                    />
+                }
+
+                {
+                    this.state.memberIndexForViewAllTasks !== -1 &&
+                    <ViewAllTasks
+                        member={this.state.members[this.state.memberIndexForViewAllTasks]}
+                        close={this.toggleViewAllTasks}
+                    />
+                }
 
             </div>
         );
